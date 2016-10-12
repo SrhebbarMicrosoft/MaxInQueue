e3fb1b5751eca7b19a7788dd0f9cebbf38302004
Srivardhan Hebbar <srhebbar@microsoft.com>
2016-10-13 03:01:16 +0530

    Working

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaxInQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine().Trim());
+            string[] tokens = Console.ReadLine().Split();
+            PriorityQueue pq = new PriorityQueue(tokens);
+            int q = int.Parse(Console.ReadLine().Trim());
+            while (q-- != 0)
+            {
+                string[] queries = Console.ReadLine().Split();
+                if (queries[0] == "1")
+                {
+                    pq.Add(queries[1]);
+                }
+                else
+                {
+                    Console.WriteLine(pq.Max);
+                }
+            }
+        }
+    }
+
+    internal class PriorityQueue
+    {
+         Queue<int> q = new Queue<int>();
+        public int Max = 0;
+
+        public PriorityQueue(string[] tokens)
+        {
+            foreach (string c in tokens)
+            {
+                Add(c);
+            }
+        }
+
+        internal void Add(string v)
+        {
+            int t = int.Parse(v);
+            if (Max < t)
+                Max = t;
+            q.Enqueue(t);
+
+        }
+    }
+
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MaxInQueue")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MaxInQueue")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5ead1fc0-956a-40d1-8f03-9223b53ea9e1")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
